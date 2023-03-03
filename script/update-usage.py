a6e039e5e6ec6f982ebe9ecfe96cae1760c9df83
Drew Volz <volz.drew@gmail.com>
2023-03-02 22:04:37 -0800

    add GitHub CI for linting
    * initalize build.yml pipeline
    * fix pylint issues

--- script/update-usage.py
+++ script/update-usage.py
@@ -1,8 +1,6 @@
 import subprocess
 
-new_usage = subprocess.run(['python3', '-m', 'costcoparser', '-h'],
-                           capture_output=True,
-                           text=True).stdout.strip('\n')
+new_usage = subprocess.run(['python3', '-m', 'costcoparser', '-h'], capture_output=True, text=True).stdout.strip('\n')
 
 start = ('<!--- START USAGE -->\n' '```shell script\n')
 end = ('\n```\n' '<!--- END USAGE -->\n')
@@ -10,11 +8,11 @@ end = ('\n```\n' '<!--- END USAGE -->\n')
 contents = ''
 
 with open('README.md', 'r') as file:
-	contents = file.read()
+    contents = file.read()
 
 old_usage = contents[contents.find(start) + len(start):contents.rfind(end)]
 
 contents = contents.replace(old_usage, new_usage)
 
 with open('README.md', 'w') as file:
-	file.write(contents)
+    file.write(contents)
